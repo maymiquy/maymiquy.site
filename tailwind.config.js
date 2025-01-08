50531abb12c8a4fda8033a2f0e9c81b60d61d009
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2025-01-09 01:34:43 +0700

    chore(config): adjusment plugins & fix tailwindcss doesnt work

---
+++ tailwind.config.js
@@ -0,0 +1,120 @@
+import { nextui } from "@nextui-org/theme";
+import typography from "@tailwindcss/typography";
+import debugScreens from "tailwindcss-debug-screens";
+import defaultTheme from "tailwindcss/defaultTheme";
+
+/** @type {import('tailwindcss').Config} */
+const tailwindConfig = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      typography: {
+        quoteless: {
+          css: {
+            "blockquote p:first-of-type::before": { content: "none" },
+            "blockquote p:first-of-type::after": { content: "none" },
+          },
+        },
+      },
+      fontFamily: {
+        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
+        display: ["var(--font-calsans)"],
+      },
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+      },
+      animation: {
+        "fade-in": "fade-in 1s ease-in-out forwards",
+        title: "title 1s ease-out forwards",
+        "fade-left": "fade-left 1s ease-in-out forwards",
+        "fade-right": "fade-right 1s ease-in-out forwards",
+        "fade-top": "fade-top 3s ease-in-out infinite",
+      },
+      keyframes: {
+        "fade-top": {
+          "0%": {
+            opacity: "100%",
+          },
+          "50%": {
+            opacity: "50%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-in": {
+          "0%": {
+            opacity: "0%",
+          },
+          "75%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        "fade-left": {
+          "0%": {
+            transform: "translateX(100%)",
+            opacity: "0%",
+          },
+
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        "fade-right": {
+          "0%": {
+            transform: "translateX(-100%)",
+            opacity: "0%",
+          },
+
+          "30%": {
+            transform: "translateX(0%)",
+            opacity: "100%",
+          },
+          "100%": {
+            opacity: "0%",
+          },
+        },
+        title: {
+          "0%": {
+            "line-height": "0%",
+            "letter-spacing": "0.25em",
+            opacity: "0",
+          },
+          "25%": {
+            "line-height": "0%",
+            opacity: "0%",
+          },
+          "80%": {
+            opacity: "100%",
+          },
+
+          "100%": {
+            "line-height": "100%",
+            opacity: "100%",
+          },
+        },
+      },
+    },
+  },
+  plugins: [
+    nextui(),
+    typography,
+    debugScreens,
+  ],
+};
+
+export default tailwindConfig;
+
