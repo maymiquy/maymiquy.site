527580317073dd54e543c41168eafa0c04b204e2
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2025-01-23 01:53:47 +0700

    chore(deps): change dependencies next ui to shadcn ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,25 +6,22 @@ import "@/styles/globals.css";
 import NextTopLoader from "nextjs-toploader";
 
 import { GithubProvider } from "@/context/github-context";
-import { NextUIProvider } from "@nextui-org/system";
 
 export default function App({ Component, pageProps }: AppProps) {
  return (
   <GithubProvider user={pageProps.user}>
-   <NextUIProvider>
-    <NextTopLoader
-     color="#ffff"
-     initialPosition={0.08}
-     crawlSpeed={50}
-     height={5}
-     crawl={true}
-     showSpinner={true}
-     easing="ease"
-     speed={50}
-     shadow="0 0 10px #ffff,0 0 5px #ffff"
-    />
-    <Component {...pageProps} />
-   </NextUIProvider>
+   <NextTopLoader
+    color="#ffff"
+    initialPosition={0.08}
+    crawlSpeed={50}
+    height={5}
+    crawl={true}
+    showSpinner={true}
+    easing="ease"
+    speed={50}
+    shadow="0 0 10px #ffff,0 0 5px #ffff"
+   />
+   <Component {...pageProps} />
   </GithubProvider>
  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
-import { nextui } from "@nextui-org/theme";
 import typography from "@tailwindcss/typography";
 import debugScreens from "tailwindcss-debug-screens";
 import defaultTheme from "tailwindcss/defaultTheme";
+import tailwindCssAnimate from "tailwindcss-animate";
 
 /** @type {import('tailwindcss').Config} */
 const tailwindConfig = {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,103 +18,155 @@ const tailwindConfig = {
       typography: {
         quoteless: {
           css: {
-            "blockquote p:first-of-type::before": { content: "none" },
-            "blockquote p:first-of-type::after": { content: "none" },
-          },
-        },
+            "blockquote p:first-of-type::before": {
+              content: "none"
+            },
+            "blockquote p:first-of-type::after": {
+              content: "none"
+            }
+          }
+        }
       },
       fontFamily: {
-        sans: ["var(--font-inter)", ...defaultTheme.fontFamily.sans],
-        display: ["var(--font-calsans)"],
+        sans: [
+          "var(--font-inter)",
+          ...defaultTheme.fontFamily.sans
+        ],
+        display: [
+          "var(--font-calsans)"
+        ]
       },
       backgroundImage: {
-        "gradient-radial":
-          "radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-radial": "radial-gradient(50% 50% at 50% 50%, var(--tw-gradient-stops))"
       },
       animation: {
         "fade-in": "fade-in 1s ease-in-out forwards",
         title: "title 1s ease-out forwards",
         "fade-left": "fade-left 1s ease-in-out forwards",
         "fade-right": "fade-right 1s ease-in-out forwards",
-        "fade-top": "fade-top 3s ease-in-out infinite",
+        "fade-top": "fade-top 3s ease-in-out infinite"
       },
       keyframes: {
         "fade-top": {
           "0%": {
-            opacity: "100%",
+            opacity: "100%"
           },
           "50%": {
-            opacity: "50%",
+            opacity: "50%"
           },
           "100%": {
-            opacity: "0%",
-          },
+            opacity: "0%"
+          }
         },
         "fade-in": {
           "0%": {
-            opacity: "0%",
+            opacity: "0%"
           },
           "75%": {
-            opacity: "0%",
+            opacity: "0%"
           },
           "100%": {
-            opacity: "100%",
-          },
+            opacity: "100%"
+          }
         },
         "fade-left": {
           "0%": {
             transform: "translateX(100%)",
-            opacity: "0%",
+            opacity: "0%"
           },
-
           "30%": {
             transform: "translateX(0%)",
-            opacity: "100%",
+            opacity: "100%"
           },
           "100%": {
-            opacity: "0%",
-          },
+            opacity: "0%"
+          }
         },
         "fade-right": {
           "0%": {
             transform: "translateX(-100%)",
-            opacity: "0%",
+            opacity: "0%"
           },
-
           "30%": {
             transform: "translateX(0%)",
-            opacity: "100%",
+            opacity: "100%"
           },
           "100%": {
-            opacity: "0%",
-          },
+            opacity: "0%"
+          }
         },
         title: {
           "0%": {
             "line-height": "0%",
             "letter-spacing": "0.25em",
-            opacity: "0",
+            opacity: "0"
           },
           "25%": {
             "line-height": "0%",
-            opacity: "0%",
+            opacity: "0%"
           },
           "80%": {
-            opacity: "100%",
+            opacity: "100%"
           },
-
           "100%": {
             "line-height": "100%",
-            opacity: "100%",
-          },
-        },
+            opacity: "100%"
+          }
+        }
       },
-    },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)"
+      },
+      colors: {
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))"
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))"
+        },
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))"
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))"
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))"
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))"
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))"
+        },
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        chart: {
+          "1": "hsl(var(--chart-1))",
+          "2": "hsl(var(--chart-2))",
+          "3": "hsl(var(--chart-3))",
+          "4": "hsl(var(--chart-4))",
+          "5": "hsl(var(--chart-5))"
+        }
+      }
+    }
   },
   plugins: [
-    nextui(),
     typography,
     debugScreens,
+    tailwindCssAnimate
   ],
 };
 
