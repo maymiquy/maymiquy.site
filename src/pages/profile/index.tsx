52d1272c7b20811d0274d449c97321251a5301ac
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2025-01-27 00:14:54 +0700

    feat(page/profile): completed add profile info page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,16 +1,37 @@
-import CommonLayout from "@/layouts/common-layout";
 import React from "react";
+import { getUser } from "@/services/user-github.service";
+import customDataJson from "@/utils/constants/personal-data.json";
 
-type Props = {};
+import { GetServerSideProps } from "next";
 
-const ProfilePage = (props: Props) => {
+import CommonLayout from "@/layouts/common-layout";
+import {
+ GithubStats,
+ HeroProfile,
+ TechStack,
+ WorkExperience,
+} from "@/components/features/profile";
+
+const ProfilePage = () => {
  return (
   <CommonLayout title="Profile">
-   <section className="h-screen items-center justify-center flex flex-col">
-    <h1 className="text-4xl font-bold">Profile</h1>
+   <section className="lg:grid lg:grid-cols-3 gap-y-20 lg:gap-6 mx-auto w-full flex flex-col mt-10 lg:mt-0 justify-center items-center">
+    <HeroProfile gridSystem="lg:col-span-2 lg:row-span-1" />
+    <TechStack gridSystem="lg:col-span-1 lg:row-span-2" />
+    <WorkExperience gridSystem="lg:col-span-2 lg:row-span-1" />
+    <GithubStats gridSystem="lg:col-span-3 lg:row-span-1" />
    </section>
   </CommonLayout>
  );
 };
+
+export const getServerSideProps = (async () => {
+ const user: GitHubUser = (await getUser(customDataJson.githubUsername)) || [];
+ return {
+  props: {
+   user,
+  },
+ };
+}) satisfies GetServerSideProps<{ user: GitHubUser }>;
 
 export default ProfilePage;
