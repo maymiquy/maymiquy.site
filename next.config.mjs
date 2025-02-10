9c688a8edb563a2c86a15fcf33a8a37c9e1f65d4
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2025-02-10 09:46:52 +0700

    chore: add optimization package barrel import & disable eslint during building on next config

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,20 @@ const nextConfig = {
       }
     ],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  experimental: {
+    optimizePackageImports: [
+      "@icons-pack/react-simple-icons",
+      "@primer/octicons-react",
+      "@/components/ui",
+      "@/components/features/common",
+      "@/components/features/home",
+      "@/components/features/profile",
+      "@/components/features/projects",
+    ],
+  }
 };
 
 export default nextConfig;
