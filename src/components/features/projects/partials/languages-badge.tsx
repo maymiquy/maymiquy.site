7472f5bcf05063829ed676ba95b9fa59eb304980
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2025-02-10 13:14:28 +0700

    feat(projects): add new component project list partials

---
+++ src/components/features/projects/partials/languages-badge.tsx
@@ -0,0 +1,174 @@
+import React from "react";
+import { getRepoLanguages } from "@/services/user-github.service";
+import {
+ SiC,
+ SiCmake,
+ SiCplusplus,
+ SiCss3,
+ SiDart,
+ SiHtml5,
+ SiIterm2,
+ SiJavascript,
+ SiKotlin,
+ SiLaravel,
+ SiNixos,
+ SiPhp,
+ SiPython,
+ SiRuby,
+ SiSwift,
+ SiTypescript,
+} from "@icons-pack/react-simple-icons";
+
+type LanguagesBadgeProps = {
+ project: any;
+};
+
+const LanguagesBadge = (props: LanguagesBadgeProps) => {
+ const [repoLanguages, setRepoLanguages] = React.useState<any>(null);
+ const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+ React.useEffect(() => {
+  const fetchRepoLanguages = async () => {
+   setIsLoading(true);
+   try {
+    const response = await getRepoLanguages(props.project.owner.login, [
+     props.project.name,
+    ]);
+    const languages = response.data.repo0.languages.edges;
+    setRepoLanguages(languages);
+   } catch (error) {
+    throw new Error(`Error while trying to retrieve languages: ${error}`);
+   } finally {
+    setTimeout(() => setIsLoading(false), 1000);
+   }
+  };
+
+  fetchRepoLanguages();
+ }, [props.project]);
+
+ if (isLoading) {
+  return <LanguagesBadgeSkeleton />;
+ }
+
+ const totalSize = repoLanguages.reduce(
+  (acc: number, lang: any) => acc + lang.size,
+  0
+ );
+
+ return (
+  <div className="flex flex-col min-w-[80px] gap-2 p-1 float-right my-6">
+   <div className="flex flex-row justify-center gap-2">
+    {repoLanguages &&
+     repoLanguages.map((lang: any, index: number) => {
+      let LanguageIcon;
+      const language = lang.node.name;
+      const color = lang.node.color;
+
+      switch (language) {
+       case "JavaScript":
+        LanguageIcon = SiJavascript;
+        break;
+       case "TypeScript":
+        LanguageIcon = SiTypescript;
+        break;
+       case "Python":
+        LanguageIcon = SiPython;
+        break;
+       case "HTML":
+        LanguageIcon = SiHtml5;
+        break;
+       case "CSS":
+        LanguageIcon = SiCss3;
+        break;
+       case "PHP":
+        LanguageIcon = SiPhp;
+        break;
+       case "Blade":
+        LanguageIcon = SiLaravel;
+        break;
+       case "Kotlin":
+        LanguageIcon = SiKotlin;
+        break;
+       case "Objective-C":
+        LanguageIcon = SiC;
+        break;
+       case "Objective-C++":
+        LanguageIcon = SiCplusplus;
+        break;
+       case "Ruby":
+        LanguageIcon = SiRuby;
+        break;
+       case "Shell":
+        LanguageIcon = SiIterm2;
+        break;
+       case "Nix":
+        LanguageIcon = SiNixos;
+        break;
+       case "Dart":
+        LanguageIcon = SiDart;
+        break;
+       case "Swift":
+        LanguageIcon = SiSwift;
+        break;
+       case "CMake":
+        LanguageIcon = SiCmake;
+        break;
+       default:
+        return null;
+      }
+
+      return (
+       <span
+        key={index}
+        className="flex items-center gap-2"
+        title={`${language}: ${((lang.size / totalSize) * 100).toFixed(1)}%`}
+       >
+        {LanguageIcon && (
+         <LanguageIcon
+          className="w-3 h-3"
+          color={color}
+          title={`${language}: ${((lang.size / totalSize) * 100).toFixed(1)}%`}
+         />
+        )}
+       </span>
+      );
+     })}
+   </div>
+   <div className="h-1 w-full bg-gray-200 rounded-full overflow-hidden">
+    {repoLanguages &&
+     repoLanguages.map((lang: any, index: number) => (
+      <div
+       key={index}
+       className="h-full"
+       style={{
+        width: `${(lang.size / totalSize) * 100}%`,
+        backgroundColor: lang.node.color,
+        float: "left",
+       }}
+       title={`${lang.node.name}: ${((lang.size / totalSize) * 100).toFixed(1)}%`}
+      />
+     ))}
+   </div>
+  </div>
+ );
+};
+
+const LanguagesBadgeSkeleton = () => {
+ return (
+  <div className="flex flex-col gap-2 p-1 float-right my-6">
+   <div className="flex flex-row justify-between gap-3">
+    {[1, 2, 3].map((index) => (
+     <div
+      key={index}
+      className="w-4 h-4 rounded-sm bg-gray-700 animate-pulse"
+     />
+    ))}
+   </div>
+   <div className="h-1 w-full bg-gray-200 rounded-full overflow-hidden">
+    <div className="h-full w-full bg-gray-700 animate-pulse" />
+   </div>
+  </div>
+ );
+};
+
+export default LanguagesBadge;
---
+++ src/components/features/projects/partials/project-article.tsx
@@ -0,0 +1,67 @@
+import Link from "next/link";
+import { SiGithub } from "@icons-pack/react-simple-icons";
+import { StarIcon } from "@primer/octicons-react";
+import LanguagesBadge from "./languages-badge";
+
+type ProjectArticleProps = {
+ project: any;
+};
+
+const ProjectArticle = (props: ProjectArticleProps) => {
+ const dynamicLink = props.project.homepage
+  ? props.project.homepage
+  : props.project.html_url;
+
+ return (
+  <article className="p-4 md:p-8">
+   <div className="flex justify-between gap-2 items-center">
+    <span className="text-xs duration-1000 text-zinc-300/70 group-hover:text-white group-hover:border-zinc-200 drop-shadow-orange">
+     <time dateTime={new Date(props.project.created_at).toISOString()}>
+      {new Date(props.project.created_at).toISOString().substring(0, 10)}
+     </time>
+    </span>
+    <span
+     title="Total stars."
+     className="text-zinc-300/70 text-sm flex flex-row items-center gap-2 "
+    >
+     <StarIcon className="w-4 h-4" />{" "}
+     {Intl.NumberFormat("en-US", { notation: "compact" }).format(
+      props.project.stargazers_count
+     )}
+    </span>
+   </div>
+
+   <Link href={dynamicLink} target="_blank">
+    <h2
+     className="z-20 text-xl font-medium duration-1000 lg:text-3xl font-display cursor-pointer"
+     title={`Click to view the ${props.project.homepage ? "app" : "repo"}.`}
+    >
+     <span className="bg-gradient-to-r from-zinc-100 to-zinc-300 hover:from-zinc-500 hover:to-zinc-700 text-transparent bg-clip-text">
+      {props.project.name}
+     </span>
+    </h2>
+   </Link>
+   <p className="z-20 mt-4 text-xs font-sans duration-1000 text-zinc-300/70">
+    {props.project.description}
+   </p>
+   <div className="flex justify-between gap-2 p-1 float-left my-6 border-t-2 border-gray-700">
+    <span
+     className="text-zinc-300/70 text-[10px] sm:text-xs font-sans"
+     title="Go to project repo ?"
+    >
+     <Link
+      href={props.project.html_url}
+      target="_blank"
+      className="hover:text-zinc-300/30 duration-200 align-middle flex items-center gap-1 -tracking-widest"
+     >
+      <SiGithub className="w-4 h-4" />
+      View project repository
+     </Link>
+    </span>
+   </div>
+   <LanguagesBadge project={props.project} />
+  </article>
+ );
+};
+
+export default ProjectArticle;
